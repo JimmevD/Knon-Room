1265c49e0ec64f91972fd901bd18f0026ab69030
JimmevD <J.van.Duijnhoven@sintlucasedu.nl>
2022-10-16 20:03:32 +0200

    Canon Ball Change

--- Knon Ball/Assets/Scripts/Canon_Ball.cs
+++ Knon Ball/Assets/Scripts/Canon_Ball.cs
@@ -3,28 +3,42 @@ using UnityEngine;
 public class Canon_Ball : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sp;
+    private int damage = 1;
 
     private void Start()
     {
         Destroy(gameObject, 2);
         rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            collision.GetComponent<Enemy>().TakeDamage(1);
+            collision.GetComponent<Enemy>().TakeDamage(damage);
             Destroy(this.gameObject);
         }
 
         if (collision.gameObject.name == "Wall Y")
         {
             rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            BounceBall();
         }
         else
         {
             rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            BounceBall();
+        }
+    }
+
+    private void BounceBall()
+    {
+        if (damage == 1)
+        {
+            sp.color = Color.yellow;
+            damage = 2;
         }
     }
 }
--- Knon Ball/Assets/Scripts/Sound_Manager.cs
+++ Knon Ball/Assets/Scripts/Sound_Manager.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Sound_Manager : MonoBehaviour
 {
     public AudioSource enemyDeadSound;
     [SerializeField] private AudioSource backgroundMusic;
     [SerializeField] private Slider volumeSlider;
+
+    //[SerializeField] private AudioMixerGroup;
 
     public void EnemyDeadSound()
     {
